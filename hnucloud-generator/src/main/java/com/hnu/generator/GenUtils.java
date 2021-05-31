1b0ea7509c1f0a052039fadf6057e206c40e01dd
adlin <812080203@qq.com>
2021-05-31 19:17:49 +0800

    各模块项目初始化构建

---
+++ hnucloud-generator/src/main/java/com/hnu/generator/GenUtils.java
@@ -0,0 +1,9 @@
+package com.hnu.generator;
+
+/**
+ * 实现代码生成的工具类
+ *
+ * @author adlin
+ */
+public class GenUtils {
+}
---
+++ hnucloud-generator/src/main/java/com/hnu/generator/VelocityUtils.java
@@ -0,0 +1,9 @@
+package com.hnu.generator;
+
+/**
+ * 模板处理工具类
+ *
+ * @author ruoyi
+ */
+public class VelocityUtils {
+}
